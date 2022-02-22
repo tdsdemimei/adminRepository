b2023a5c86bc39f42e2fba18d3a2d2a2060a81e9
adminRepository <1794055180@qq.com>
2022-02-22 10:57:09 +0800

    第二次提交

--- src/com/tjw/Student.java
+++ src/com/tjw/Student.java
@@ -1,5 +1,19 @@
 package com.tjw;
 
-public class Student {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Student {
+    public static void main(String[] args) {
+        StringBuilder x = new StringBuilder("9");
+        List list = new ArrayList();
+        Long count = 0l ;
+        for (int i = 0; i < 9; i++) {
+            long s = Long.valueOf(String.valueOf(x));
+            count += s ;
+            x.append("9");
+        }
+
+        System.out.println(count);
+    }
 }
